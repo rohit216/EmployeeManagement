b56674af0d43c8fe887bc4f3a123703d6334e898
rohitpro <rohitkhot1997@gmail.com>
2018-05-08 18:34:48 +0530

    New Changes

--- src/employeemanagementsystem/EmployeesJFrame.java
+++ src/employeemanagementsystem/EmployeesJFrame.java
@@ -1,10 +1,4 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package employeemanagementsystem;
-
 import java.sql.SQLException;
 import java.util.logging.Level;
 import java.util.logging.Logger;
@@ -14,10 +8,6 @@ import javax.swing.event.ListSelectionEvent;
 import javax.swing.table.DefaultTableCellRenderer;
 import net.sf.jasperreports.engine.JRException;
 
-/**
- *
- * @author Christine Targa
- */
 public class EmployeesJFrame extends javax.swing.JFrame {
 
     boolean addRecord = false;
--- src/employeemanagementsystem/JasperReports.java
+++ src/employeemanagementsystem/JasperReports.java
@@ -1,14 +1,4 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package employeemanagementsystem;
-
-/**
- *
- * @author Christine Targa
- */
 
 import java.io.InputStream;
 import java.sql.SQLException;
